f62d450aa7407fcf98a20031ae717053e6b7db42
Jhea Prudencio <2232799@slu.edu.ph>
2024-11-23 10:18:21 +0800

    initial smoke test

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  timeout: 60000, // Timeout is shared between all tests.
+  projects: [
+    {
+      name: 'Smoke',
+      testMatch: /.*smoke.spec.ts/,
+      retries: 0,
+    },
+    {
+      name: 'Default',
+      testIgnore: /.*smoke.spec.ts/,
+      retries: 2,
+    },
+  ],
+});
